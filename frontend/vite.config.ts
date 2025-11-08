5795d03f8277b091c4b876ec2855c66f6fda2d9e
Matt Decker <mdeckerr@Matts-MBP.lan>
2025-11-08 11:37:44 -0600

    003 Chore: Connect Frontend and Backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5279',
+                changeOrigin: true,
+                secure: false,
+            }
+        }
+    }
 })
